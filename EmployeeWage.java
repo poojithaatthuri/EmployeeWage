d1f3ef600a6b447220192b896aa9a46011e440b0
poojitha atthuri <atthuripoojapavani@gmail.com>
2022-05-29 23:42:05 +0530

    added:logic to check attendance

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,7 +1,15 @@
 package com.employeewages;
-
+import java.util.Random;
 public class EmployeeWage {
     public static void main(String[] args){
         System.out.println("Welcom to Employee Wage Computation Program");
+        Random random = new Random();
+        int attendance = random.nextInt(2);
+        if(attendance == 1)
+            System.out.println("Employee present...");
+        else
+            System.out.println("Employee Absent....");
+
+
     }
 }
