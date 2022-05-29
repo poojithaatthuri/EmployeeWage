ae3077d04759d6c79a3b97b19985d6aecb9df29b
poojitha atthuri <atthuripoojapavani@gmail.com>
2022-05-30 00:22:24 +0530

    added:logic using switch case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -7,7 +7,18 @@ public class EmployeeWage {
         int attendance = random.nextInt(2);
         int wagePerHour=20;
         int fullDayHours=8;
+        int partTimeHours=4;
         int wagePerDay = 0;
+
+        switch(attendance)
+        {
+            case 0: System.out.println("Employee Absent..");
+                     break;
+            case 1: wagePerDay = partTimeHours * wagePerHour;
+                     break;
+            default:
+                wagePerDay = fullDayHours * wagePerHour;
+        }
 
         if(attendance == 1) {
             System.out.println("Employee present...");
