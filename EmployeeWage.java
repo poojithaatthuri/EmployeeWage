453980b746cde50ebed96ed02c2c8d8249485c07
poojitha atthuri <atthuripoojapavani@gmail.com>
2022-05-29 23:59:01 +0530

    added:logic to check part time employee wages

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,10 +5,17 @@ public class EmployeeWage {
         System.out.println("Welcom to Employee Wage Computation Program");
         Random random = new Random();
         int attendance = random.nextInt(2);
-        if(attendance == 1)
+        int wagePerHour=20;
+        int fullDayHours=8;
+        int wagePerDay = 0;
+
+        if(attendance == 1) {
             System.out.println("Employee present...");
+            wagePerDay = wagePerHour * fullDayHours;
+        }
         else
             System.out.println("Employee Absent....");
+        System.out.println("Employee wages Per Day: " + wagePerDay);
 
 
     }
